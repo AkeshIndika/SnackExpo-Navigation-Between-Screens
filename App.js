5cf13fb396ef75f29014a935840019c31383838e
AkeshIndika <AkeshIndika@example.com>
2023-08-24 19:09:18 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { Pressable, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+function Home({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Welcome to our Home Screen</Text>
+      <Pressable
+        onPress={() => navigation.navigate('Conference')}
+        style={{ backgroundColor: 'plum', padding: 10, marginBottom: 10, marginTop: 10 }}
+      >
+      <Text>Conference</Text>
+      </Pressable>
+      <Pressable
+        onPress={() => navigation.navigate('Story')}
+        style={{ backgroundColor: 'plum', padding: 10 }}
+      >
+       <Text>Story</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+function Conference({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Conference Details</Text>
+    </View>
+  );
+}
+
+function Story() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Our Story</Text>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+     <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Logging Page" component={Home} />
+        <Stack.Screen name="Conference" component={Conference} />
+        <Stack.Screen name="Story" component={Story} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
